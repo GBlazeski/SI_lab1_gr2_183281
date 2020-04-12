c6159aa4761ccdbf21ba7c390692767db96671ab
GBlazeski <ccgypls@gmail.com>
2020-04-12 20:41:53 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -9,14 +9,51 @@ class Student {
 		index = "";
 		firstName = "";
 		lastName = "";
+		grades = new int[]{0};
 	}
 	//TODO seters & getters
+	public String getIndex(){
+		return index;
+	}
 
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public grades[] getGrades()
+	{
+		return grades[];
+	}
+	public void setFirstName(String newFirstName)
+	{
+		this.firstName = newFirstName;
+	}
+	public void setIndex(String newIndex)
+	{
+		this.index= newIndex;
+	}
+	public void setLasttName(String newLastName)
+	{
+		this.lastName = newLirstName;
+	}
+	public void setGrades(grades[] grades )
+	{
+		this.grades = grades;
+	}
 	public double getAverage() {
-		//TODO
+		double avg = 0.0;
+		int sum = 0;
+
+		return avg;
+
 	}
 
 	public int ECTSCredits() {
-		//TODO
-	}
+		double ects = 0.0;
+		return ects;
+		}
 }
