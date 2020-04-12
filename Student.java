d5153d660e62cab93f19e0e0894e2a60207bcf2a
GBlazeski <ccgypls@gmail.com>
2020-04-12 20:22:08 +0200

    grades list added

--- Student.java
+++ Student.java
@@ -2,9 +2,14 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
-
+	int grades[100];
 	//TODO constructor
-
+	public Student()
+	{
+		index = "";
+		firstName = "";
+		lastName = "";
+	}
 	//TODO seters & getters
 
 	public double getAverage() {
